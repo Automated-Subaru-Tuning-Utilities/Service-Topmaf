0e766d1192326a83be0aefb1ce095075fd758c28
WDominic <dom0_o@protonmail.com>
2022-08-19 17:04:30 -0400

    error fixed: dont be a dumbass

--- topmaf/topmaf_calc.py
+++ topmaf/topmaf_calc.py
@@ -7,18 +7,21 @@ from topmaf_api_models import topmaf_input
 from maf_voltages import maf_voltages
 
 def main(data: topmaf_input):
+    #turn input data into dataframes
     targets = [i.dict() for i in data.target_afr]
-    print(targets)
-    print(type(targets)) # outputs <class 'list'>
-    for i in targets:
-        print(i)
-    print(type(targets[0])) # outputs <class 'dict'>
-    df = pd.DataFrame(targets) # outputs numpy.int64 not iterable 
-                               # TF?!??!?
-    return targets[1]["load"] # yet this works
-    # log_data = pd.DataFrame(data.log_data)
-    # target_afr = data.target_afr
-    # log_data = data.log_data
+    targets = pd.DataFrame(targets)
+    log_data = [i.dict() for i in data.log_data]
+    log_data = pd.DataFrame(log_data)
+    
+    #for testing
+    # print(targets)
+    # print(log_data)
+    print(targets.loc[0,"target_afr"])
+    print(targets.loc[1,"rpm"])
 
+    print(log_data.loc[0,"wideband_o2"])
+    print(log_data.loc[0,"load"])
+
+    return {"result": "success"}
 if __name__ == "__main__":
     print("testing here")
