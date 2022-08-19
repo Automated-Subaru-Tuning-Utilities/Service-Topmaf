5fc611cb888b41026905135055542864f6e7a04d
WDominic <dom0_o@protonmail.com>
2022-08-19 16:49:12 -0400

    another error

--- topmaf/topmaf_calc.py
+++ topmaf/topmaf_calc.py
@@ -10,10 +10,12 @@ def main(data: topmaf_input):
     targets = [i.dict() for i in data.target_afr]
     print(targets)
     print(type(targets)) # outputs <class 'list'>
+    for i in targets:
+        print(i)
     print(type(targets[0])) # outputs <class 'dict'>
     df = pd.DataFrame(targets) # outputs numpy.int64 not iterable 
                                # TF?!??!?
-    return df
+    return targets[1]["load"] # yet this works
     # log_data = pd.DataFrame(data.log_data)
     # target_afr = data.target_afr
     # log_data = data.log_data
