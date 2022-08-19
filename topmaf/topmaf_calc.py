6dbf5309b211c41527df2c2d908d7c857bd75ae5
WDominic <dom0_o@protonmail.com>
2022-08-19 16:37:13 -0400

    this sum bullshit fr

--- topmaf/topmaf_calc.py
+++ topmaf/topmaf_calc.py
@@ -6,13 +6,17 @@ sys.path.append("./models")
 from topmaf_api_models import topmaf_input
 from maf_voltages import maf_voltages
 
-def main(data: list[topmaf_input]):
-    target_afr = data.target_afr
-    target_afr = pd.DataFrame([item.dict() for item in target_afr])
+def main(data: topmaf_input):
+    targets = [i.dict() for i in data.target_afr]
+    print(targets)
+    print(type(targets)) # outputs <class 'list'>
+    print(type(targets[0])) # outputs <class 'dict'>
+    df = pd.DataFrame(targets) # outputs numpy.int64 not iterable 
+                               # TF?!??!?
+    return df
     # log_data = pd.DataFrame(data.log_data)
     # target_afr = data.target_afr
     # log_data = data.log_data
-    return target_afr
 
 if __name__ == "__main__":
     print("testing here")
